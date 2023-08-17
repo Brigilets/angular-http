6d3e7d4e29478c5b4678cab0d913c3fc0b27d5e4
Brigilets <b.sabutyte@gmail.com>
2023-08-17 15:00:57 +0200

    initial commit: implmented post & get methods with types

---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.http
+      .post<{ name: string }>(
+        'https://ng-http-a25f7-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>(
+        'https://ng-http-a25f7-default-rtdb.europe-west1.firebasedatabase.app/posts.json'
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({
+                ...responseData[key as keyof Object],
+                id: key,
+              });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((posts) => {
+        // ...
+        console.log(posts);
+      });
+  }
+}
