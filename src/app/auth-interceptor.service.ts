fc397bcaed0076382d8d5db765cf0168a36654ac
Brigilets <b.sabutyte@gmail.com>
2023-08-18 10:33:42 +0200

    added logging interceptor and registered multiple interceptors in app.module.ts

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -9,22 +9,12 @@ import { tap } from 'rxjs';
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // runs before intercept leaves the app we can log or modify request
-    console.log("Request is on it's way");
 
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz'),
     });
     // allows the request to continue the jurney & must be returned
     // return next.handle(req);
-    return next.handle(modifiedRequest).pipe(
-      tap((event) => {
-        // must always use event inside interceptor tap()
-        console.log('all events from iterceptor', event);
-        if (event.type === HttpEventType.Response) {
-          console.log('Response arrived, body data: ');
-          console.log(event.body);
-        }
-      })
-    );
+    return next.handle(modifiedRequest);
   }
 }
